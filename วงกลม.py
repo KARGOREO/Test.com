df6e2f010401cd605b8147f8cbe28de87e343e47
NAN <oppwerq@gmail.com>
2021-03-24 18:42:12 +0700

    GG5

--- วงกลม.py
+++ วงกลม.py
@@ -1,6 +1,7 @@
 import math
+def V3(): 
+    radius = 7
+    area = math.pi * (radius**2)
  
-radius = 7
-area = math.pi * (radius**2)
- 
-print(f"พื้นที่ของวงกลม คือ {area:.2f}")
+    print(f"พื้นที่ของวงกลม คือ {area:.2f}")
+V3()    
